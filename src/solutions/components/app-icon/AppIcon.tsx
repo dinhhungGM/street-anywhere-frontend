2ed8aa411ce9a19f9d7fdd4aaeb0bd84d67f58ea
duyphuong02 <18521276@gm.uit.edu.vn>
2022-09-13 21:19:03 +0700

    rename icon to app-icon

--- src/solutions/components/app-icon/AppIcon.tsx
+++ src/solutions/components/app-icon/AppIcon.tsx
@@ -6,8 +6,8 @@ type IconProps = {
   color?: string;
 };
 
-const Icon = ({ component, fontSize = 22, color = '#84849d' }: IconProps) => {
+const AppIcon = ({ component, fontSize = 22, color = '#84849d' }: IconProps) => {
   return <SvgIcon component={component} inheritViewBox sx={{ fontSize, color }} />;
 };
 
-export default Icon;
+export default AppIcon;
