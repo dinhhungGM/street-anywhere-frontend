dab65110c2fcb6ac7153922a9e5969f40ff7b049
duyphuong02 <18521276@gm.uit.edu.vn>
2022-09-15 00:57:28 +0700

    add navigator

--- src/features/create-new-post/CreateNewPost.tsx
+++ src/features/create-new-post/CreateNewPost.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppMap } from '../../solutions/components/app-map';
 
 const CreateNewPost = () => {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log('Latitude is :', position.coords.latitude);
+      console.log('Longitude is :', position.coords.longitude);
+    });
+  }, []);
+
   return (
     <>
       <AppMap />
