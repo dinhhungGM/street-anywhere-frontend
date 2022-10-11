d7adf4a6bc34f4cd659834ea9bd8557dccaa643e
duyphuong02 <18521276@gm.uit.edu.vn>
2022-10-12 00:12:57 +0700

    update category model

--- src/features/categories/store/categoriesModels.ts
+++ src/features/categories/store/categoriesModels.ts
@@ -1,4 +1,4 @@
 export interface ICategory {
   id: number;
-  categoryName: number;
+  categoryName: string;
 }
--- src/features/categories/store/index.ts
+++ src/features/categories/store/index.ts
@@ -1,10 +1,12 @@
 import categoriesSlice from './categoriesSlice';
 import * as categoriesSelectors from './categoriesSelectors';
 import * as categoriesModels from './categoriesModels';
+import * as categoriesAsyncActions from './categoriesAsyncActions';
 
 const categoriesReducer = categoriesSlice.reducer;
 const categoriesActions = {
   ...categoriesSlice.actions,
+  ...categoriesAsyncActions,
 };
 
 export { categoriesReducer, categoriesActions, categoriesSelectors, categoriesModels };
