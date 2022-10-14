17415dc02ac33f5650c47e170ae1e27ebe3728af
duyphuong02 <18521276@gm.uit.edu.vn>
2022-10-14 11:21:27 +0700

    add hashtags management

---
+++ src/features/admin-dashboard/components/hashtags-management/HashTagsManagement.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HashTagsManagement = () => {
+  return <div>HashTagsManagement</div>;
+};
+
+export default HashTagsManagement;
---
+++ src/features/admin-dashboard/components/hashtags-management/index.ts
@@ -0,0 +1,1 @@
+export { default as HashTagsManagement } from './HashTagsManagement';
