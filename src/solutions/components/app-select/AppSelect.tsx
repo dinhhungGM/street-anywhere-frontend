2b01b4df984149df89ac04c4caee5500206c137e
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-11-11 15:41:50 +0700

    create component AppSelect

---
+++ src/solutions/components/app-select/AppSelect.tsx
@@ -0,0 +1,43 @@
+import { Autocomplete, Box, TextField } from '@mui/material';
+import React from 'react';
+
+interface IAppSelect {
+  data: any;
+  value: any;
+  optionLabel?: string;
+  mappingLabelField: string;
+  isMultipleSelect?: boolean;
+  onChange: (event: any, newValue: any) => void;
+}
+
+const AppSelect = ({
+  data,
+  value = undefined,
+  mappingLabelField,
+  optionLabel = 'Label',
+  isMultipleSelect = false,
+  onChange,
+}: IAppSelect) => {
+  return (
+    <>
+      <Box
+        sx={{
+          width: '100%',
+        }}
+      >
+        <Autocomplete
+          fullWidth
+          disablePortal
+          value={value}
+          options={data}
+          onChange={onChange}
+          multiple={isMultipleSelect}
+          getOptionLabel={(optionItem) => optionItem[mappingLabelField]}
+          renderInput={(params) => <TextField {...params} variant='standard' label={optionLabel} />}
+        />
+      </Box>
+    </>
+  );
+};
+
+export default React.memo(AppSelect);
