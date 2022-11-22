8a9d8cdb26fe43e9d62598c962d9b368a18dc5a9
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-11-23 01:49:55 +0700

    install lib for carousel

---
+++ src/solutions/components/app-carousel/AppCarousel.tsx
@@ -0,0 +1,43 @@
+import { memo } from 'react';
+import Slider from 'react-slick';
+
+interface IAppCarouselProps {
+  settings?: any;
+}
+const AppCarousel = ({
+  settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  },
+}: IAppCarouselProps) => {
+  return (
+    <>
+      <Slider {...settings}>
+        {' '}
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </>
+  );
+};
+
+export default memo(AppCarousel);
