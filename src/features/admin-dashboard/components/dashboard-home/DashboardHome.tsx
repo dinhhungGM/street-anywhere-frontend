32445bd3dc1b77d59ad1b2a6dc391a9d7b19936c
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-12-07 09:28:57 +0700

    install serve lib and update admin dashboard

--- src/features/admin-dashboard/components/dashboard-home/DashboardHome.tsx
+++ src/features/admin-dashboard/components/dashboard-home/DashboardHome.tsx
@@ -1,14 +1,37 @@
-import { Box, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import { useEffect } from 'react';
 import styles from './styles.module.scss';
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
+import { adminActions, adminSelectors } from '../../store';
+import AppStatsByYear from '../../../../solutions/components/app-stats-by-year/AppStatsByYear';
 
 const DashboardHome = () => {
+  const dispatch = useAppDispatch();
+  const usersByYears = useAppSelector(adminSelectors.selectUsersByYears);
+  const postsByYears = useAppSelector(adminSelectors.selectPostsByYears);
+
+  useEffect(() => {
+    dispatch(adminActions.getStatsOfUserByYear());
+    dispatch(adminActions.getStatsOfPostsByYear());
+  }, []);
   return (
     <>
       <Box className={styles.home}>
         <Typography variant='h4' fontWeight={700} className={styles.home__greeting}>
           Hi, Welcome back!
         </Typography>
+        <Grid container spacing={4} alignItems='center' justifyContent='center'>
+          <Grid item md={6} xs={12} sm={12}>
+            <Box width='100%' height='100%'>
+              {usersByYears && <AppStatsByYear dataByYears={usersByYears} chartTitle='Number of new users by month' />}
+            </Box>
+          </Grid>
+          <Grid item md={6} xs={12} sm={12}>
+            <Box width='100%' height='100%'>
+              {postsByYears && <AppStatsByYear dataByYears={postsByYears} chartTitle='Number of posts by month' />}
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
     </>
   );
--- src/features/admin-dashboard/store/adminSelectors.ts
+++ src/features/admin-dashboard/store/adminSelectors.ts
@@ -1,8 +1,11 @@
 import { RootState } from '../../../app/store';
 import { createSelector } from '@reduxjs/toolkit';
+
 const selectAdminStateFeature = (rootState: RootState) => rootState.admin;
 export const selectAllUsers = createSelector(selectAdminStateFeature, (admin) => admin.users);
 export const selectAllRoles = createSelector(selectAdminStateFeature, (admin) => admin.roles);
 export const selectAllReactions = createSelector(selectAdminStateFeature, (admin) => admin.reactions);
 export const selectAllCategories = createSelector(selectAdminStateFeature, (admin) => admin.categories);
 export const selectAllHashTags = createSelector(selectAdminStateFeature, (admin) => admin.hashTags);
+export const selectUsersByYears = createSelector(selectAdminStateFeature, (admin) => admin.usersByYears);
+export const selectPostsByYears = createSelector(selectAdminStateFeature, (admin) => admin.postsByYears);
