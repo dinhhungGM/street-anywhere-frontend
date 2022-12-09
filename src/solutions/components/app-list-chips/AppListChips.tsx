9ac59840028def0e38cae8cd0a7ff5395e272dab
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-12-10 01:26:51 +0700

    create AppListChips component

---
+++ src/solutions/components/app-list-chips/AppListChips.tsx
@@ -0,0 +1,31 @@
+import { Chip, Stack, Typography } from '@mui/material';
+import { memo } from 'react';
+import { AppIcon } from '../app-icon';
+
+interface IAppListChipsProps {
+  data?: string[];
+  icon?: any;
+  title?: string;
+  iconColor?: string;
+}
+const AppListChips = ({ data = [], icon, title, iconColor = null }: IAppListChipsProps) => {
+  return (
+    <>
+      {data && data.length && (
+        <Stack direction='row' alignItems='center'>
+          <AppIcon icon={icon} color={iconColor} />
+          <Typography fontWeight={700} marginLeft={1}>
+            {title}:
+          </Typography>
+          <Stack direction='row' alignItems='center' justifyContent='flex-start' spacing={1} marginLeft={1}>
+            {data.map((item, idx) => (
+              <Chip key={idx} label={item} />
+            ))}
+          </Stack>
+        </Stack>
+      )}
+    </>
+  );
+};
+
+export default memo(AppListChips);
