30315463e7b9a059a253e8920c28a4a3f2c036d7
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-12-13 20:29:43 +0700

    create AppInputWithEdit

---
+++ src/solutions/components/app-input-with-edit/AppInputWithEdit.tsx
@@ -0,0 +1,51 @@
+import { Edit } from '@mui/icons-material';
+import { Box, Stack, TextField, Typography } from '@mui/material';
+import { memo } from 'react';
+import { AppIcon } from '../app-icon';
+import { AppIconButton } from '../app-icon-button';
+import styles from './styles.module.scss';
+
+interface IAppInputWithEditProps {
+  value?: any;
+  title?: string;
+  inputType?: string;
+  isReadonly?: boolean;
+}
+const AppInputWithEdit = ({
+  value,
+  title = 'Title',
+  inputType = 'text',
+  isReadonly = false,
+}: IAppInputWithEditProps) => {
+  return (
+    <>
+      <Box
+        sx={{
+          minWidth: '100%',
+        }}>
+        <Stack direction='row' alignItems='center' spacing={2}>
+          <Typography fontWeight={700} className={styles.title}>
+            {title}:{' '}
+          </Typography>
+          <Stack direction='row' alignItems='center' width='85%' className={styles.input}>
+            <TextField
+              type={inputType}
+              value={value}
+              fullWidth
+              sx={{
+                '& fieldset': { border: 'none' },
+                '& .Mui-disabled': {
+                  WebkitTextFillColor: '#000',
+                },
+              }}
+              disabled
+            />
+            {!isReadonly && <AppIconButton tooltip='Edit' icon={<AppIcon icon={Edit} color='success' />} />}
+          </Stack>
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default memo(AppInputWithEdit);
