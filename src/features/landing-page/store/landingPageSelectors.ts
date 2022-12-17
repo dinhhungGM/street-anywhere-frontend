544b58fbe3b8a993d39ed303ac1ab16fecafca83
Linhttn1000 <18521000@gm.uit.edu.vn>
2022-12-17 17:10:45 +0700

    add load more function

--- src/features/landing-page/store/landingPageSelectors.ts
+++ src/features/landing-page/store/landingPageSelectors.ts
@@ -2,5 +2,15 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../../../app/store';
 const selectLandingPageFeature = (rootState: RootState) => rootState.landingPage;
 
-export const selectPosts = createSelector(selectLandingPageFeature, (landingPage) => landingPage.posts);
-export const selectTopPosts = createSelector(selectLandingPageFeature, (landingPage) => landingPage.topPosts);
+export const selectPosts = createSelector(
+  selectLandingPageFeature,
+  (landingPage) => landingPage.posts,
+);
+export const selectTopPosts = createSelector(
+  selectLandingPageFeature,
+  (landingPage) => landingPage.topPosts,
+);
+export const selectTotalPage = createSelector(
+  selectLandingPageFeature,
+  (landingPage) => landingPage.totalPage,
+);
