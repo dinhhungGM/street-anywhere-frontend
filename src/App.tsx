afe6b4567070c486b4743a39ba09fd1c580a69a6
Linhttn1000 <18521000@gm.uit.edu.vn>
2023-01-03 14:26:47 +0700

    add fallback for loading lazy component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@emotion/react';
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { Box, CircularProgress } from '@mui/material';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router';
@@ -8,7 +9,6 @@ import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import './App.css';
 import { Wrapper } from './features/wrapper';
-import LoadingSpinner from './solutions/components/loading-spinner/LoadingSpinner';
 import routes from './solutions/configs/routes';
 import { appTheme } from './solutions/configs/theme';
 
@@ -17,7 +17,12 @@ function App() {
     <>
       <ThemeProvider theme={appTheme}>
         <Wrapper>
-          <Suspense fallback={<LoadingSpinner />}>
+          <Suspense
+            fallback={
+              <Box className='fallback'>
+                <CircularProgress color='primary' />
+              </Box>
+            }>
             <Routes>
               {routes.map((route) => {
                 if (route.children && route.children.length) {
